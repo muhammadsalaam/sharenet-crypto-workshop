a5ad30e4354da0ccdaa6f41bbb7a8967f438cdb6
muhammadsalaam <muhammads@ct-international.co.za>
2022-05-25 13:15:31 +0200

    update

--- src/components/BookingModal.vue
+++ src/components/BookingModal.vue
@@ -1,28 +1,93 @@
 <template>
   <div class="container">
-    <h1>Availability for {{ " " + city }}</h1>
-    <div class="row">
-      <div class="col-4" v-for="(dates, index) in dates" :key="index">
-        <div class="date-card">
-          <span class="dates ms-4 pt-3"
-            ><strong>{{ dates }}</strong></span
-          >
-          <Button
-            @click="toggleDate(dates)"
-            icon="pi pi-calendar-plus"
-            label="Book Now"
-            class="p-button-rounded ms-4 mt-2"
-            style="width: 12rem"
-          />
+    <div v-if="city === 'Cape Town'">
+      <h1>Availability for {{ " " + city }}</h1>
+      <div class="row">
+        <div class="col-4" v-for="cape in cptAvail" :key="cape">
+          <div class="date-card">
+            <span class="dates ms-4 pt-3"
+              ><strong>{{ cape.date }}</strong></span
+            ><br />
+            <span class="status ms-4 pt-3"
+              ><strong>{{ cape.remaining + " seats left" }}</strong></span
+            >
+            <Button
+              @click="toggleDate(cape.date)"
+              icon="pi pi-calendar-plus"
+              label="Book Now"
+              class="p-button-rounded ms-4 mt-2"
+              style="width: 12rem"
+            />
+          </div>
         </div>
       </div>
+      <Button
+        @click="toggleCardsActive"
+        label="Go Back"
+        class="p-button-rounded ms-4 mt-2"
+        style="width: 12rem"
+      />
     </div>
-    <Button
-      @click="toggleCardsActive"
-      label="Go Back"
-      class="p-button-rounded ms-4 mt-2"
-      style="width: 12rem"
-    />
+    <!-- Johannesburg -->
+
+    <div v-if="city === 'Johannesburg'">
+      <h1>Availability for {{ " " + city }}</h1>
+      <div class="row">
+        <div class="col-4" v-for="joburg in jhbAvail" :key="joburg">
+          <div class="date-card">
+            <span class="dates ms-4 pt-3"
+              ><strong>{{ joburg.date }}</strong></span
+            ><br />
+            <span class="status ms-4 pt-3"
+              ><strong>{{ joburg.remaining + " seats left" }}</strong></span
+            >
+            <Button
+              @click="toggleDate(joburg.date)"
+              icon="pi pi-calendar-plus"
+              label="Book Now"
+              class="p-button-rounded ms-4 mt-2"
+              style="width: 12rem"
+            />
+          </div>
+        </div>
+      </div>
+      <Button
+        @click="toggleCardsActive"
+        label="Go Back"
+        class="p-button-rounded ms-4 mt-2"
+        style="width: 12rem"
+      />
+    </div>
+    <!-- Durban  -->
+
+    <div v-if="city === 'Durban'">
+      <h1>Availability for {{ " " + city }}</h1>
+      <div class="row">
+        <div class="col-4" v-for="durbs in dbnAvail" :key="durbs">
+          <div class="date-card">
+            <span class="dates ms-4 pt-3"
+              ><strong>{{ durbs.date }}</strong></span
+            ><br />
+            <span class="status ms-4 pt-3"
+              ><strong>{{ durbs.remaining + " seats left" }}</strong></span
+            >
+            <Button
+              @click="toggleDate(durbs.date)"
+              icon="pi pi-calendar-plus"
+              label="Book Now"
+              class="p-button-rounded ms-4 mt-2"
+              style="width: 12rem"
+            />
+          </div>
+        </div>
+      </div>
+      <Button
+        @click="toggleCardsActive"
+        label="Go Back"
+        class="p-button-rounded ms-4 mt-2"
+        style="width: 12rem"
+      />
+    </div>
   </div>
 
   <SeatsModal v-if="isModalOpen" />
@@ -36,7 +101,11 @@ import SeatsModal from "./SeatsModal.vue";
 export default {
   name: "BookingModal",
   data() {
-    return {};
+    return {
+      remainCPT: [],
+      remainJHB: [],
+      remainDBN: [],
+    };
   },
   components: {
     Button,
@@ -48,10 +117,29 @@ export default {
       city: (state) => state.booking.city,
       venue: (state) => state.booking.venue,
       isModalOpen: (state) => state.booking.isModalOpen,
+      cptAvail: (state) => state.booking.cptAvail,
+      jhbAvail: (state) => state.booking.jhbAvail,
+      dbnAvail: (state) => state.booking.dbnAvail,
     }),
   },
   methods: {
     ...mapMutations(["toggleModal", "toggleCardsActive", "toggleDate"]),
+    setRemainder() {
+      this.remainCPT[0] = this.cptAvail[0].remaining;
+      this.remainCPT[1] = this.cptAvail[1].remaining;
+      this.remainCPT[2] = this.cptAvail[2].remaining;
+
+      this.remainJHB[0] = this.jhbAvail[0].remaining;
+      this.remainJHB[1] = this.jhbAvail[1].remaining;
+      this.remainJHB[2] = this.jhbAvail[2].remaining;
+
+      this.remainDBN[0] = this.dbnAvail[0].remaining;
+      this.remainDBN[1] = this.dbnAvail[1].remaining;
+      this.remainDBN[2] = this.dbnAvail[2].remaining;
+    },
+  },
+  beforeMount() {
+    this.setRemainder();
   },
 };
 </script>
--- src/components/SeatsModal.vue
+++ src/components/SeatsModal.vue
@@ -11,42 +11,40 @@
     <h3 class="subheading">Date: {{ date }}</h3>
     <h4 class="venue">Venue: {{ venue }}</h4>
     <section id="seats">
-      <input
-        id="seat-1"
-        class="seat-select"
-        type="checkbox"
-        value="1"
-        name="seat[]"
-      />
-      <label for="seat-1" class="seat"></label>
-      <input
-        id="seat-2"
-        class="seat-select"
-        type="checkbox"
-        value="2"
-        name="seat[]"
-      />
-      <label for="seat-2" class="seat"></label>
-      <input
-        id="seat-3"
-        class="seat-select"
-        type="checkbox"
-        value="3"
-        name="seat[]"
-      />
-      <label for="seat-3" class="seat"></label>
-      <input
-        id="seat-4"
-        class="seat-select"
-        type="checkbox"
-        value="4"
-        name="seat[]"
-      />
-      <label for="seat-4" class="seat"></label>
+      <div class="container">
+        <div class="row">
+          <div
+            class="form-check col-4"
+            v-for="(seat, index) in seatArr"
+            :key="index"
+          >
+            <input
+              type="checkbox"
+              class="form-check-input"
+              :checked="checkboxCheck(seat)"
+              v-model="seatArr[index]"
+              :name="index"
+              true-value="1"
+              false-value="0"
+            />
+            <label class="form-check-label" :for="index">{{
+              "Seat " + (index + 1)
+            }}</label>
+            <hr />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="booked">
+      <p class="booked-seat"></p>
     </section>
     <template #footer>
       <div class="flex justify-content-end">
-        <Button label="Confirm" @click="confirmCart" class="p-button-primary" />
+        <Button
+          label="Confirm"
+          @click="confirmSeats"
+          class="p-button-primary"
+        />
       </div>
     </template>
   </Dialog>
@@ -61,6 +59,7 @@ export default {
   data() {
     return {
       display: true,
+      seatArr: [],
     };
   },
   computed: {
@@ -68,6 +67,10 @@ export default {
       date: (state) => state.booking.date,
       venue: (state) => state.booking.venue,
       city: (state) => state.booking.city,
+      cptAvail: (state) => state.booking.cptAvail,
+      jhbAvail: (state) => state.booking.jhbAvail,
+      dbnAvail: (state) => state.booking.dbnAvail,
+      remainder: (state) => state.booking.remainder,
     }),
   },
   components: {
@@ -79,25 +82,92 @@ export default {
     confirmCart() {
       this.toggleModal();
     },
+    splitSeats() {
+      // run if city is Cape Town
+      if (this.city === "Cape Town") {
+        for (let i = 0; i < 3; i++) {
+          if (this.cptAvail[i].date === this.date) {
+            this.seatArr = this.cptAvail[i].seats;
+            console.log(this.seatArr);
+          }
+        }
+      }
+      // run if city is Johannesburg
+      if (this.city === "Johannesburg") {
+        for (let i = 0; i < 3; i++) {
+          if (this.jhbAvail[i].date === this.date) {
+            this.seatArr = this.jhbAvail[i].seats;
+            console.log(this.seatArr);
+          }
+        }
+      }
+      // run if city is Durban
+      if (this.city === "Durban") {
+        for (let i = 0; i < 3; i++) {
+          if (this.dbnAvail[i].date === this.date) {
+            this.seatArr = this.dbnAvail[i].seats;
+            console.log(this.seatArr);
+          }
+        }
+      }
+    },
+    checkboxCheck(seat) {
+      if (seat === 1) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    confirmSeats() {
+      // run if city is Cape Town
+      if (this.city === "Cape Town") {
+        for (let i = 0; i < 3; i++) {
+          if (this.cptAvail[i].date === this.date) {
+            this.cptAvail[i].seats = this.seatArr;
+            this.cptAvail[i].remaining = 0;
+            for (let j = 0; j < this.cptAvail[i].seats.length; j++) {
+              if (this.cptAvail[i].seats[j] === 0) {
+                this.cptAvail[i].remaining++;
+              }
+            }
+          }
+        }
+      }
+      // run if city is Johannesburg
+      if (this.city === "Johannesburg") {
+        for (let i = 0; i < 3; i++) {
+          if (this.jhbAvail[i].date === this.date) {
+            this.jhbAvail[i].seats = this.seatArr;
+            this.jhbAvail[i].remaining = 0;
+            for (let j = 0; j < this.jhbAvail[i].seats.length; j++) {
+              if (this.jhbAvail[i].seats[j] === 0) {
+                this.jhbAvail[i].remaining++;
+              }
+            }
+          }
+        }
+      }
+      // run if city is Durban
+      if (this.city === "Durban") {
+        for (let i = 0; i < 3; i++) {
+          if (this.dbnAvail[i].date === this.date) {
+            this.dbnAvail[i].seats = this.seatArr;
+            this.dbnAvail[i].remaining = 0;
+            for (let j = 0; j < this.dbnAvail[i].seats.length; j++) {
+              if (this.dbnAvail[i].seats[j] === 0) {
+                this.dbnAvail[i].remaining++;
+              }
+            }
+          }
+        }
+      }
+      this.toggleModal();
+    },
+  },
+  beforeMount() {
+    this.splitSeats();
   },
 };
 </script>
 
-<style scoped>
-.seat {
-  float: left;
-  display: block;
-  margin: 5px;
-  background: #4caf50;
-  width: 100px;
-  height: 140px;
-}
-
-.seat-select {
-  display: none;
-}
-
-.seat-select:checked + .seat {
-  background: #f44336;
-}
-</style>
+<style scoped></style>
